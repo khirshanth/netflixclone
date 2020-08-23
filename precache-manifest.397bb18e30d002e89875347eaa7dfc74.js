407c31a8739b71ece721e6718b052972ed47504a
Krishna Murugan <murugesan@think42labs.com>
2020-08-23 10:51:33 +0530

    Updates

---
+++ precache-manifest.397bb18e30d002e89875347eaa7dfc74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51887e78d9be595b9890d4deec8cf4cb",
+    "url": "/khirshanth/netflixclone/index.html"
+  },
+  {
+    "revision": "ebc8cb60e25e21bbbaca",
+    "url": "/khirshanth/netflixclone/static/css/main.eda9eaeb.chunk.css"
+  },
+  {
+    "revision": "e86be0948bdc5f077418",
+    "url": "/khirshanth/netflixclone/static/js/2.6802548d.chunk.js"
+  },
+  {
+    "revision": "499d2627123fe885bda0003637163daa",
+    "url": "/khirshanth/netflixclone/static/js/2.6802548d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc8cb60e25e21bbbaca",
+    "url": "/khirshanth/netflixclone/static/js/main.c0221639.chunk.js"
+  },
+  {
+    "revision": "337932509ca0cf30c032",
+    "url": "/khirshanth/netflixclone/static/js/runtime-main.d3a31384.js"
+  }
+]);
